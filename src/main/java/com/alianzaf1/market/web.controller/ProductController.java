c7ab147248da22c5c0d4b49d3480eee393f35fbf
TaraPadilla <daalejito20@gmail.com>
2024-08-16 21:31:10 -0500

    Implementa Controller

---
+++ src/main/java/com/alianzaf1/market/web.controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.alianzaf1.market.web.controller;
+
+import com.alianzaf1.market.domain.Product;
+import com.alianzaf1.market.domain.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    public List<Product> getAll(){
+       return productService.getAll();
+    }
+
+    public Optional<Product> getProduct(int productId){
+        return productService.getProduct(productId);
+    }
+
+    public List<Product> getByCategoria(int categoryId){
+        return productService.getByCategoria(categoryId);
+    }
+
+    public Product save(Product product){
+        return productService.save(product);
+    }
+
+    public boolean delete(int productId) {
+        return productService.delete(productId);
+    }
+}
